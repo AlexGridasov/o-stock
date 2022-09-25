695a5bc2af3f5c2165fd1a392c4f94b9f0330198
Alex Gridasov <alexgridasov@users.noreply.github.com>
2022-09-25 22:51:43 +0300

    Licensing Service: Read configuration from Config Server

--- licensing-service/src/main/java/com/gri/alex/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/gri/alex/LicenseServiceApplication.java
@@ -2,13 +2,14 @@ package com.gri.alex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LicenseServiceApplication {
 
     public static void main(String[] args) {
